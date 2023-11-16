9b69472ebd96415dde992a69d74d49c6b67c6acb
Jake Fulmine <jake@fulmine.xyz>
2023-11-16 08:41:28 +0100

    feat: massive login hero on the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,25 @@
-<button class="btn">hi</button>
+<div class="hero bg-base-200">
+  <div class="hero-content my-8 flex-col lg:flex-row-reverse">
+    <div class="text-center lg:text-left">
+      <h1 class="text-5xl font-bold">Welcome to the new dashboard!</h1>
+      <p class="pt-6">Oh yeah. This rewrite is totally going to be finished (massive sarcasm).</p>
+      <p class="pb-6">TODO: explain what the dash is here?</p>
+    </div>
+    <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form class="card-body">
+        <div class="form-control">
+          <label for="token-input" class="label">
+            <span class="label-text">PluralKit API token</span>
+          </label>
+          <input type="password" placeholder="token" id="token-input" class="input input-bordered" />
+          <label class="label" for="token-input">
+            <span class="label-text-alt">You can get this by running pk;token</span>
+          </label>
+        </div>
+        <div class="form-control mt-6">
+          <button class="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
