5599f8a7a43e23a99275c985cce23aadc9b95eca
Jake Fulmine <jake@fulmine.xyz>
2023-12-09 02:52:12 +0100

    fix: only show recovery settings once logged in

--- src/components/SettingsSideBar.svelte
+++ src/components/SettingsSideBar.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+  import type { System } from "$api/types"
   import { IconPaint, IconLayout, IconEyeHeart, IconAmbulance } from "@tabler/icons-svelte"
+  import { getContext } from "svelte"
+
+  const system: System = getContext<System>("system") || null
 </script>
 
 <ul class="menu p-4 w-64 lg:w-80 min-h-full bg-base-200 text-base-content">
@@ -8,7 +12,9 @@
   <li><a href="/settings/theme"><IconPaint /> Theme</a></li>
   <li><a href="/settings/layout"><IconLayout /> Layout</a></li>
   <li><a href="/settings/accessibility"><IconEyeHeart /> Accessibility</a></li>
-  <li>
-    <a href="/settings/recovery"><span class="text-error"><IconAmbulance /></span> Recovery</a>
-  </li>
+  {#if system}
+    <li>
+      <a href="/settings/recovery"><span class="text-error"><IconAmbulance /></span> Recovery</a>
+    </li>
+  {/if}
 </ul>
