9b7b2dedff8d10462a80c8eacb400b16ceb5c134
Jake Fulmine <jake@fulmine.xyz>
2024-04-08 07:25:17 +0200

    add error page to dash

---
+++ src/routes/dash/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { page } from "$app/stores"
+</script>
+
+<main class="prose">
+  <h1>{$page.status}: {$page.error?.message}</h1>
+  {#if $page.status === 401}
+    <p>You need to be logged in to access this page. You can <a href="/">log in on the homepage</a>.</p>
+  {/if}
+</main>
