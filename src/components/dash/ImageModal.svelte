e33bb6034546c10af3fff8548721d0363afa2649
Jake Fulmine <jake@fulmine.xyz>
2024-04-09 08:33:42 +0200

    fix: images overflowing on small screens

--- src/components/dash/ImageModal.svelte
+++ src/components/dash/ImageModal.svelte
@@ -12,7 +12,7 @@
 <dialog bind:this={element} class="modal">
   <form method="dialog" class="w-full h-full">
     <button class="w-full h-full grid justify-center items-center p-4 max-h-screen max-w-screen">
-      <img class="rounded-xl m-auto w-auto max-w-3xl h-auto max-h-full" src={url} alt={`${itemName}'s ${imageType}`} />
+      <img class="rounded-xl m-auto w-full max-w-3xl h-auto max-h-full" src={url} alt={`${itemName}'s ${imageType}`} />
     </button>
   </form>
 </dialog>
