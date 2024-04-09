56b1f7f8c7b69b4712731fe3f3345571b22b59a7
Jake Fulmine <jake@fulmine.xyz>
2024-04-09 09:15:54 +0200

    feat: add commit hash to bottom of page

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,17 @@
+<script lang="ts">
+  import { PUBLIC_REPOSITORY_URL } from "$env/static/public"
+
+  const version = __COMMIT_HASH__.slice(1, __COMMIT_HASH__.length - 1)
+</script>
+
 <footer class="footer items-center p-4">
   <nav class="grid-flow-col gap-4">
-    <span>Commit: [commit here]</span>
+    <span>Commit: <a
+      class="underline"
+      href={`${
+        PUBLIC_REPOSITORY_URL ? PUBLIC_REPOSITORY_URL : "https://github.com/Draconizations/pk-dashboard-sveltekit"
+      }/commit/${version}`}>{version}</a>
+    </span>
   </nav>
   <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
     <a class="link-hover" href="/about">About</a>
