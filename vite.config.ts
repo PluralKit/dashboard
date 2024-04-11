e7c98750d499d5f3a66b7872c788cbbcbfe9ea0e
Jake Fulmine <jake@fulmine.xyz>
2024-04-11 20:33:44 +0200

    don't error on local commonjs modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import commonjs from "vite-plugin-commonjs"
 import { defineConfig } from 'vite';
 import { execSync } from "node:child_process"
 
 const hash = execSync("git rev-parse --short HEAD").toString().trim()
 
 export default defineConfig({ 
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), commonjs()],
 	define: {
 		__COMMIT_HASH__: JSON.stringify("_" + hash)
 	}
