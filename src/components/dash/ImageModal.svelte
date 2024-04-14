9477c9f0a34a9e09cd4d9b64bcb78cd417567c41
Jake Fulmine <jake@fulmine.xyz>
2024-04-14 14:09:23 +0200

    fix: don't do keyboard navigation on closed modals

--- src/components/dash/ImageModal.svelte
+++ src/components/dash/ImageModal.svelte
@@ -9,9 +9,9 @@
 </script>
 
 <button class="btn btn-neutral btn-sm w-full" onclick={() => element.showModal()}>View {imageType}</button>
-<dialog bind:this={element} class="modal">
+<dialog tabindex="-1" bind:this={element} class="modal">
   <form method="dialog" class="w-full h-full">
-    <button class="w-full h-full grid justify-center items-center p-4 max-h-screen max-w-screen">
+    <button tabindex="-1" class="w-full h-full grid justify-center items-center p-4 max-h-screen max-w-screen">
       <div class="max-w-3xl h-full flex flex-col">
         <img class="rounded-xl m-auto w-auto h-auto max-h-full" src={url} alt={`${itemName}'s ${imageType}`} />
       </div>
