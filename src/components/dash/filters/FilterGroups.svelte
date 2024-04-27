f16eb5a35647d3cf7f86143fe6e7c4a5a8da6f9c
Jake Fulmine <jake@fulmine.xyz>
2024-04-27 13:17:11 +0200

    fix hydration error
    was it really that simple.

--- src/components/dash/filters/FilterGroups.svelte
+++ src/components/dash/filters/FilterGroups.svelte
@@ -145,125 +145,127 @@
         onconsider={(e) => handleConsiderFilter(e, group.id)}
         onfinalize={(e) => handleFinalFilter(e, group.id)}
       >
-        {#each group.filters as filter, i (filter.id)}
-          <div
-            class="bg-base-100 p-3 flex flex-col rounded-lg hover:border-primary border-muted/50 outline-primary border-2 gap-1 relative"
-            aria-label={`${filter.fieldName} filter: ${filter.mode}`}
-          >
-            {#if filter.field === "group"}
-              <FilterHeader action={removeFilter(group.id, filter.id)}>
-                <span class="text-sm">
-                  {#if groupArrayModes.includes(filter.mode)}
-                    members with <b>groups</b> that {filterModeText(filter.mode, filter.valueType)
-                      .verb}...
-                  {:else if filter.mode === FilterMode.HIGHERTHAN}
-                    members with more than {filter.value} <b>groups</b>
-                  {:else if filter.mode === FilterMode.LOWERTHAN}
-                    members with less than {filter.value} <b>groups</b>
-                  {:else if filter.mode === FilterMode.EMPTY}
-                    members without a <b>group</b>
-                  {:else if filter.mode === FilterMode.NOTEMPTY}
-                    members with a <b>group</b>
-                  {/if}
-                </span>
-              </FilterHeader>
-              {#if groupArrayModes.includes(filter.mode)}
-                <Svelecte
-                  class="svelecte-control-pk w-full"
-                  options={dash.groups.list.options}
-                  multiple
-                  valueField="value"
-                  labelField="text"
-                  value={filter.value}
-                  on:change={(e) => {
+        {#if group.filters.length > 0}
+          {#each group.filters as filter, i (filter.id)}
+            <div
+              class="bg-base-100 p-3 flex flex-col rounded-lg hover:border-primary border-muted/50 outline-primary border-2 gap-1 relative"
+              aria-label={`${filter.fieldName} filter: ${filter.mode}`}
+            >
+              {#if filter.field === "group"}
+                <FilterHeader action={removeFilter(group.id, filter.id)}>
+                  <span class="text-sm">
+                    {#if groupArrayModes.includes(filter.mode)}
+                      members with <b>groups</b> that {filterModeText(filter.mode, filter.valueType)
+                        .verb}...
+                    {:else if filter.mode === FilterMode.HIGHERTHAN}
+                      members with more than {filter.value} <b>groups</b>
+                    {:else if filter.mode === FilterMode.LOWERTHAN}
+                      members with less than {filter.value} <b>groups</b>
+                    {:else if filter.mode === FilterMode.EMPTY}
+                      members without a <b>group</b>
+                    {:else if filter.mode === FilterMode.NOTEMPTY}
+                      members with a <b>group</b>
+                    {/if}
+                  </span>
+                </FilterHeader>
+                {#if groupArrayModes.includes(filter.mode)}
+                  <Svelecte
+                    class="svelecte-control-pk w-full"
+                    options={dash.groups.list.options}
+                    multiple
+                    valueField="value"
+                    labelField="text"
+                    value={filter.value}
+                    on:change={(e) => {
                     list.filters[index].filters[i].value = e.detail.map((i: any) => i.value)
                     list.process(dash.groups.list.raw)
                     list.paginate()
                   }}
-                />
-              {:else if filter.valueType === "number"}
-                <input
-                  class="input input-sm input-bordered"
-                  placeholder={`Filter by groups...`}
-                  type="number"
-                  value={filter.value}
-                  min={0}
-                  onchange={(e) => updateFilterValue(e, index, i, true)}
-                />
-              {/if}
-            {:else if filter.field === "member"}
-              <FilterHeader action={removeFilter(group.id, filter.id)}>
-                <span class="text-sm">
-                  {#if groupArrayModes.includes(filter.mode)}
-                    groups with <b>members</b> that {filterModeText(filter.mode, filter.valueType)
-                      .verb}...
-                  {:else if filter.mode === FilterMode.HIGHERTHAN}
-                    groups with more than {filter.value} <b>members</b>
-                  {:else if filter.mode === FilterMode.LOWERTHAN}
-                    groups with less than {filter.value} <b>members</b>
-                  {:else if filter.mode === FilterMode.EMPTY}
-                    groups without a <b>members</b>
-                  {:else if filter.mode === FilterMode.NOTEMPTY}
-                    groups with a <b>members</b>
-                  {/if}
-                </span>
-              </FilterHeader>
-              {#if groupArrayModes.includes(filter.mode)}
-                <Svelecte
-                  class="svelecte-control-pk w-full"
-                  options={dash.members.list.options}
-                  multiple
-                  valueField="value"
-                  labelField="text"
-                  value={filter.value}
-                  on:change={(e) => {
+                  />
+                {:else if filter.valueType === "number"}
+                  <input
+                    class="input input-sm input-bordered"
+                    placeholder={`Filter by groups...`}
+                    type="number"
+                    value={filter.value}
+                    min={0}
+                    onchange={(e) => updateFilterValue(e, index, i, true)}
+                  />
+                {/if}
+              {:else if filter.field === "member"}
+                <FilterHeader action={removeFilter(group.id, filter.id)}>
+                  <span class="text-sm">
+                    {#if groupArrayModes.includes(filter.mode)}
+                      groups with <b>members</b> that {filterModeText(filter.mode, filter.valueType)
+                        .verb}...
+                    {:else if filter.mode === FilterMode.HIGHERTHAN}
+                      groups with more than {filter.value} <b>members</b>
+                    {:else if filter.mode === FilterMode.LOWERTHAN}
+                      groups with less than {filter.value} <b>members</b>
+                    {:else if filter.mode === FilterMode.EMPTY}
+                      groups without a <b>members</b>
+                    {:else if filter.mode === FilterMode.NOTEMPTY}
+                      groups with a <b>members</b>
+                    {/if}
+                  </span>
+                </FilterHeader>
+                {#if groupArrayModes.includes(filter.mode)}
+                  <Svelecte
+                    class="svelecte-control-pk w-full"
+                    options={dash.members.list.options}
+                    multiple
+                    valueField="value"
+                    labelField="text"
+                    value={filter.value}
+                    on:change={(e) => {
                     list.filters[index].filters[i].value = e.detail.map((i: any) => i.value)
                     list.process(dash.groups.list.raw)
                     list.paginate()
                   }}
-                />
-              {:else if filter.valueType === "number"}
-                <input
-                  class="input input-sm input-bordered"
-                  placeholder={`Filter by members...`}
-                  type="number"
-                  value={filter.value}
-                  min={0}
-                  onchange={(e) => updateFilterValue(e, index, i, true)}
-                />
+                  />
+                {:else if filter.valueType === "number"}
+                  <input
+                    class="input input-sm input-bordered"
+                    placeholder={`Filter by members...`}
+                    type="number"
+                    value={filter.value}
+                    min={0}
+                    onchange={(e) => updateFilterValue(e, index, i, true)}
+                  />
+                {/if}
+              {:else}
+                <FilterHeader action={removeFilter(group.id, filter.id)}>
+                  <span class="text-sm"
+                    ><b>{filter.fieldName}s</b> that {filterModeText(filter.mode, filter.valueType)
+                      .verb}
+                    {filter.value !== null && filter.value !== ""
+                      ? `${filter.valueType === "string" ? `"${filter.value}"` : filter.value}`
+                      : "..."}
+                    {filterModeText(filter.mode, filterFieldType(filter.field)).afterVerb}</span
+                  >
+                </FilterHeader>
+                {#if filter.valueType === "number"}
+                  <input
+                    class="input input-sm input-bordered"
+                    placeholder={`Filter by ${filter.fieldName}...`}
+                    type="number"
+                    value={filter.value}
+                    min={0}
+                    onchange={(e) => updateFilterValue(e, index, i, true)}
+                  />
+                {:else if filter.valueType === "string"}
+                  <input
+                    class="input input-sm input-bordered"
+                    placeholder={`Filter by ${filter.fieldName}...`}
+                    type="text"
+                    value={filter.value}
+                    onchange={(e) => updateFilterValue(e, index, i, false)}
+                  />
+                {/if}
               {/if}
-            {:else}
-              <FilterHeader action={removeFilter(group.id, filter.id)}>
-                <span class="text-sm"
-                  ><b>{filter.fieldName}s</b> that {filterModeText(filter.mode, filter.valueType)
-                    .verb}
-                  {filter.value !== null && filter.value !== ""
-                    ? `${filter.valueType === "string" ? `"${filter.value}"` : filter.value}`
-                    : "..."}
-                  {filterModeText(filter.mode, filterFieldType(filter.field)).afterVerb}</span
-                >
-              </FilterHeader>
-              {#if filter.valueType === "number"}
-                <input
-                  class="input input-sm input-bordered"
-                  placeholder={`Filter by ${filter.fieldName}...`}
-                  type="number"
-                  value={filter.value}
-                  min={0}
-                  onchange={(e) => updateFilterValue(e, index, i, true)}
-                />
-              {:else if filter.valueType === "string"}
-                <input
-                  class="input input-sm input-bordered"
-                  placeholder={`Filter by ${filter.fieldName}...`}
-                  type="text"
-                  value={filter.value}
-                  onchange={(e) => updateFilterValue(e, index, i, false)}
-                />
-              {/if}
-            {/if}
-          </div>
-        {/each}
+            </div>
+          {/each}
+        {/if}
       </div>
     </div>
   {/each}
