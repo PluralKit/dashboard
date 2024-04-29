ab417300f2bae5e852d1bcfeba74dd722c535d0c
Jake Fulmine <jake@fulmine.xyz>
2024-04-29 10:14:45 +0200

    tweak: only update image preview onchange

--- src/components/dash/edit/EditImage.svelte
+++ src/components/dash/edit/EditImage.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import type { Group, Member, System } from "$api/types"
   import { IconPencil } from "@tabler/icons-svelte"
-  // @ts-ignore
-  import { autoresize } from "svelte-textarea-autoresize"
 
   let {
     original,
@@ -15,6 +13,8 @@
     item: Member|Group|System
     field: string
   } = $props()
+
+  let previewValue = $state(value)
 </script>
 
 <div class="flex flex-col">
@@ -26,10 +26,11 @@
       type="url"
       placeholder={original}
       class="flex-1 input input-bordered placeholder:text-base-content/40"
+      onchange={() => previewValue = value}
     />
     {#if value}
       <div class="avatar w-12 h-12">
-        <img src={value} class="object-cover rounded-full" alt={`${field} preview`} />
+        <img src={previewValue} class="object-cover rounded-full" alt={`${field} preview`} />
       </div>
     {/if}
     <span title={original === value ? "" : "Edited"}>
