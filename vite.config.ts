7314ef0212a6daba5d4d96dda4516af98ff09245
Jake Fulmine <jake@fulmine.xyz>
2024-06-09 00:01:48 +0200

    chore: update & optimize tabler-icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,45 @@ import { sveltekit } from "@sveltejs/kit/vite"
 import commonjs from "vite-plugin-commonjs"
 import { defineConfig } from "vite"
 import { execSync } from "node:child_process"
+import MagicString from "magic-string"
 
 const hash = execSync("git rev-parse --short HEAD").toString().trim()
 
 export default defineConfig({
-  plugins: [sveltekit(), commonjs()],
+  plugins: [sveltekit(), commonjs(), tablerOptimizer()],
   define: {
     __COMMIT_HASH__: JSON.stringify("_" + hash),
   },
 })
+
+// taken from https://github.com/tabler/tabler-icons/issues/669#issuecomment-1993756128
+// converts all named imports to direct imports during transforming
+function tablerOptimizer(): import("vite").Plugin {
+	return {
+		name: "tabler-svelte optimizer",
+		transform(code, id) {
+			const ms = new MagicString(code, { filename: id })
+			ms.replace(
+				/([ \t]*)import\s+\{([^;]*?)\}\s+from\s+['"]@tabler\/icons-svelte['"];/g,
+				(match, whitespace: string, importNames: string) => {
+					const hasSemi = match.endsWith(";")
+					const imports = importNames
+						.split(",")
+						.map((v) => v.trim())
+						.map((name) => {
+							const path = name
+							return `${whitespace}import ${name} from '@tabler/icons-svelte/${path}.svelte'${hasSemi ? ";" : ""}`
+						})
+					return imports.join("\n")
+				}
+			)
+
+			if (ms.hasChanged()) {
+				return {
+					code: ms.toString(),
+					map: ms.generateMap()
+				}
+			}
+		}
+	}
+}
